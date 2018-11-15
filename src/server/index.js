519248e0d8960adef7d335f2f9cd24c92bdf89b3
Pavel_Yatskevich <pyatskevich@gmail.com>
2018-11-15 14:48:28 +0300

    add ssr

---
+++ src/server/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from '../App';
+
+require('babel-register')({
+  presets: ['env']
+});
+
+const app = express();
+
+function htmlTemplate(reactDom) {
+  return `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>React SSR</title>
+        </head>
+
+        <body>
+            <div id="app">${reactDom}</div>
+        </body>
+        </html>
+    `;
+}
+
+app.get('/*', (req, res) => {
+  const jsx = (<App />);
+  const reactDom = renderToString(jsx);
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(htmlTemplate(reactDom));
+});
+
+app.listen(2048);
